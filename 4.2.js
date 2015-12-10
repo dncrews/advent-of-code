47812e68eda06361b76c14fcbf4d583a87dc2cf1
Dan Crews <crewsd@gmail.com>
2015-12-10 11:22:01 -0700

    Day 4

---
+++ 4.2.js
@@ -0,0 +1,16 @@
+/* eslint-env es6 */
+'use strict';
+
+const crypto = require('crypto');
+
+const input = `bgvyzdsv`;
+
+let i = 1;
+let hash;
+
+while(i++) {
+  hash = crypto.createHash('md5').update(input + i).digest('hex');
+  if (hash.indexOf('000000') === 0) break;
+}
+
+console.log(i, hash);
